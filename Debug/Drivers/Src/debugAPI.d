f40ce4768a56da1120ba9911545c8514864ebd02
truong <truongdtu02@gmail.com>
2021-12-26 00:29:36 +0700

    add gitignore

--- Debug/Drivers/Src/debugAPI.d
+++
@@ -1,4 +0,0 @@
-Drivers/Src/debugAPI.o: ../Drivers/Src/debugAPI.c \
- E:/truyenthanhproject/STM32CubeIDE/workspace_1.6.1/sim7600LL/Drivers/Inc/debugAPI.h
-
-E:/truyenthanhproject/STM32CubeIDE/workspace_1.6.1/sim7600LL/Drivers/Inc/debugAPI.h:
--- Debug/Drivers/Src/md5.d
+++
@@ -1,4 +0,0 @@
-Drivers/Src/md5.o: ../Drivers/Src/md5.c \
- E:/truyenthanhproject/STM32CubeIDE/workspace_1.6.1/sim7600LL/Drivers/Inc/md5.h
-
-E:/truyenthanhproject/STM32CubeIDE/workspace_1.6.1/sim7600LL/Drivers/Inc/md5.h:
